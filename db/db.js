5b7363fc1b90192e5c701b479939846390cc6c80
vishsec <vishsecures@gmail.com>
2024-07-04 11:19:46 +0530

    DB Functionalities

--- db/db.js
+++ db/db.js
@@ -1,9 +1,24 @@
 
 require('dotenv').config();
 const mongoose = require("mongoose");
+const User = require("../models/userModel")
 
 const connect = async ()=>{
     await mongoose.connect(process.env.mongo)
 };
 
-module.exports = { connect };
+const disconnect = async () => {
+    return await mongoose.connection.close()
+}
+
+const findUser = async (obj) => { //json obj
+    await User.findOne(obj)
+}
+
+const saveUser = async (newUser) => {
+    return await newUser.save();
+}
+
+
+
+module.exports = { connect, disconnect, findUser, saveUser };
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,60 +1,16 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message:"successful get",
-        metadata:{
-            hostname:req.hostname,
-            method: req.method,
-        },
-    });
+const router = express.Router()
+
+router.post('/register', (req, res, next) => {
+
+
+
+
+
+    
 })
 
-router.get('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: "succesful get by ID",
-        metadata:{
-        hostname:req.hostname,
-        userid:req.params.id,
-        method: req.method,
-        }
-    })
-})
-
-router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message:"successful post",
-        metadata:{
-            name: req.body.name,
-            hostname:req.hostname,
-            method: req.method,
-        },
-    });
-})
-
-router.put('/:id', (req, res, next) => {
-    res.status(201).json({
-        message: "succeful put by Id",
-        metadata:{
-        
-            hostname:req.hostname,
-            userid:req.params.id,
-            method: req.method,
-        }
-    })
-})
-
-router.delete('/:id', (req, res, next) => {
-    res.status(201).json({
-        message: "successful delete by Id",
-        metadata:{  
-            hostname:req.hostname,
-            userid:req.params.id,
-            method: req.method,
-        }
-    })
-})
 
 module.exports = router;
